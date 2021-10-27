88cdb7a65c7560caada8f357cd40e3fe20c0e5a8
Canoga <91224366+Canoga@users.noreply.github.com>
2021-10-27 19:56:26 +0200

    First changes to the server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const { response } = require('express');
+const path = require('path')
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+app.use(express.static(path.join(__dirname, '/static')));
+// app.get('/',(request, response) => {
+//     response.sendFile(path.join(__dirname, '/static/index.html'));
+// })
+
+// app.get('/speakers',(request, response) => {
+//     response.sendFile(path.join(__dirname, '/static/speakers.html'));
+// })
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+})
