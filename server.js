7445b411c678af0df664df1931e851d53bf6cd5d
Canoga <91224366+Canoga@users.noreply.github.com>
2021-10-28 18:02:46 +0200

    Adding ejs, does not work yet

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
-app.use(express.static(path.join(__dirname, '/static')));
-// app.get('/',(request, response) => {
-//     response.sendFile(path.join(__dirname, '/static/index.html'));
-// })
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
 
-// app.get('/speakers',(request, response) => {
-//     response.sendFile(path.join(__dirname, '/static/speakers.html'));
-// })
+const port = 3000;
+app.use(express.static(path.join(__dirname, './static')));
+app.get('/',(request, response) => {
+    response.render('pages/index', {pageTitle: 'Welcome'});
+})
+
+app.get('/speakers',(request, response) => {
+    response.sendFile(path.join(__dirname, '/static/speakers.html'));
+})
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
