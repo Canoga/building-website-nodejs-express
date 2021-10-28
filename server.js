d440572a2f72110539d48cd8de4618720fa18ead
Canoga <91224366+Canoga@users.noreply.github.com>
2021-10-28 18:19:43 +0200

    Now at least it works

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'))
+app.set('views', path.join(__dirname, './views'));
 
 const port = 3000;
 app.use(express.static(path.join(__dirname, './static')));
-app.get('/',(request, response) => {
-    response.render('pages/index', {pageTitle: 'Welcome'});
-})
+app.get('/', (request, response) => {
+  response.render('pages/index', { pageTitle: 'Welcome' });
+});
 
-app.get('/speakers',(request, response) => {
-    response.sendFile(path.join(__dirname, '/static/speakers.html'));
-})
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, '/static/speakers.html'));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
